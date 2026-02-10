6562fb7669d0188567f5edd42d2f4d609deabdfe
Danilo Pelin <danilocanabanopelinjr@gmail.com>
2026-02-10 16:51:22 +0800

    Setup tailwindcss and tanstack router by installing using the terminal window.

--- map-world/src/App.tsx
+++ map-world/src/App.tsx
@@ -1,5 +1,18 @@
-export default function App() {
-  return <>
-    
-    </>
+import './App.css';
+import { createRouter } from '@tanstack/react-router';
+import { routeTree } from './routeTree.gen';
+import { RouterProvider } from '@tanstack/react-router';
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
 }
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
--- map-world/vite.config.ts
+++ map-world/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { tanstackRouter } from "@tanstack/router-vite-plugin"
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    tailwindcss(),
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
